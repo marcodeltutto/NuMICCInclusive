ef808e1ca1d16b9f276977db997db2dc9b3fc1bd
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2016-07-27 11:07:42 +0100

    Updating runjob.sh.

--- runjob.sh
+++ runjob.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
 
-echo "Running runjobs.sh" 
+echo "Type DoSubmit to submit jobs to the cluster." 
 
-echo qsub -l cput=10:00:00 -l walltime=10:00:00 job_NuMICCInclusive.sh
-qsub -l cput=10:00:00 -l walltime=10:00:00 job_NuMICCInclusive.sh 
+
+logpath=/home/deltutto/NuMICCInclusive/logs
+mkdir -p $logpath
+outdir=/home/deltutto/NuMICCInclusive/outputs
+mkdir -p $outdir
+
+
+DoSubmit() {
+
+  namebase="numiccinclusive"
+
+  rm $logpath/*
+
+  elog=$logpath/e_${namebase}.txt
+  olog=$logpath/o_${namebase}.txt
+  echo
+  echo Submitting job now.
+  echo
+  echo  
+  echo qsub -l cput=10:00:00 -l walltime=10:00:00 job_NuMICCInclusive.sh
+  qsub -l cput=10:00:00 -l walltime=10:00:00 job_NuMICCInclusive.sh
+  echo
+
+}
+
+
 
 
 

e94b7d5c3f20aa1f7988fba553c784042705950f
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2016-07-28 16:17:19 +0100

    Make Class now with root6

--- makeClass.C
+++ makeClass.C
@@ -1,8 +1,9 @@
 void makeClass() {
 
   TFile *f = new TFile("/data/t2k/lar/uboone/prodgenie_numi_nu_uboone_MCC7/prodgenie_numi_nu_cosmic_uboone_merged_gen_g4_detsim_reco1_reco2_ana.root");
-  f->cd("analysistree");
+  //f->cd("analysistree");
   std::cout << "here" << std::endl;
-  TTree *v = (TTree*)f->Get("anatree");
-  anatree->MakeClass("AnaNuMI");
+  TTree *v = (TTree*)f->Get("analysistree/anatree");
+  std::cout << "here2" << std::endl;
+  v->MakeClass("AnaNuMI");
 }

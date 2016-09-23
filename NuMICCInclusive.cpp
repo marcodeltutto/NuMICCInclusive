de6d6c3f7aeddd052b5ae954b35a8631721f2233
Marco Del Tutto <marco.deltutto@physics.ox.ac.uk>
2016-09-23 11:39:48 -0500

    Adding intermidiate plots for each selecion step.

--- NuMICCInclusive.cpp
+++ NuMICCInclusive.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
+#include <string>
 #include <ctime>
+#include <vector>
 
 #include <TRandom1.h>
 #include <TSystem.h>
@@ -15,11 +17,67 @@
 
 using namespace std;
 
+//...
+struct CutPlots {
+
+  Spectrum* Snuenergy_numu;
+  Spectrum* Snuenergy_anumu;
+  Spectrum* Snuenergy_nue;
+  Spectrum* Snuenergy_nc;
+  Spectrum* Snuenergy_cosmics;
+
+};
+
+void InstantiateIntermidiatePlots(std::map<std::string,CutPlots> &cutToPlotsMap, double totalPOT) {
+
+  std::vector<std::string> cutname;
+  cutname.resize(2);
+  cutname.at(0) = "flashtag";
+  cutname.at(1) = "vertexcontained";
+
+  for (int i = 0; i < cutname.size(); i++) {
+    CutPlots cutplots;
+    cutplots.Snuenergy_numu  = new Spectrum("nuenergy_numu_"+cutname.at(i),  "#nu_{#mu};Neutrino Energy [GeV];Selected Events up to "+cutname.at(i), 100, 0, 10, totalPOT);
+    cutplots.Snuenergy_anumu = new Spectrum("nuenergy_anumu_"+cutname.at(i), "#bar{#nu}_{#mu};Neutrino Energy [GeV];Selected Events up to "+cutname.at(i), 100, 0, 10, totalPOT);
+    cutplots.Snuenergy_nue   = new Spectrum("nuenergy_nue_"+cutname.at(i),   "#nu_{e}/#bar{#nu}_{e};Neutrino Energy [GeV];Selected Events up to "+cutname.at(i), 100, 0, 10, totalPOT);
+    cutplots.Snuenergy_nc    = new Spectrum("nuenergy_nc_"+cutname.at(i),    "NC;Neutrino Energy [GeV];Selected Events up to "+cutname.at(i), 100, 0, 10, totalPOT);
+
+    cutToPlotsMap.emplace(cutname[i],cutplots);
+  }
+}
+
+void MakeIntermidiatePlots(CutPlots cutplots, AnaNuMI * anatree){
+
+  if(anatree->ccnc_truth[0] == 0 || anatree->nuPDG_truth[0]==14) cutplots.Snuenergy_numu   -> Fill(anatree->enu_truth[0]);
+  if(anatree->ccnc_truth[0] == 0 || anatree->nuPDG_truth[0]==-14) cutplots.Snuenergy_anumu -> Fill(anatree->enu_truth[0]);
+  if(anatree->ccnc_truth[0] == 0 || anatree->nuPDG_truth[0]==12 || anatree->nuPDG_truth[0]==-12) cutplots.Snuenergy_nue -> Fill(anatree->enu_truth[0]);
+  if(anatree->ccnc_truth[0] == 1) cutplots.Snuenergy_nc -> Fill(anatree->enu_truth[0]);
+
+}
+
+void SaveIntermidiatePlots(std::map<std::string,CutPlots> &cutToPlotsMap) {
+
+  for (std::map<std::string,CutPlots>::iterator it=cutToPlotsMap.begin(); it!=cutToPlotsMap.end(); ++it) {
+    std::cout << "Saving plot " << it->first << endl;
+    CutPlots cutplots = it->second;
+    cutplots.Snuenergy_numu ->Save();
+    cutplots.Snuenergy_anumu->Save();
+    cutplots.Snuenergy_nue  ->Save();
+    cutplots.Snuenergy_nc   ->Save();
+
+  }
+
+}
 
 
 int main(int argc, char* argv[]) {
 
   clock_t begin = clock();
+
+  if (argc != 3) {
+    cout << "Provide two arguments!" << endl;
+    exit(0);
+  }
 
   string beam  = argv[1];
   string dopot = argv[2];
@@ -74,6 +132,13 @@ int main(int argc, char* argv[]) {
   std::string TrackProdName  = "pandoraNu";
   std::string VertexProdName = "pandoraNu";
 
+  //Create a new file + a clone of old tree in new file
+  TFile *oldfile = new TFile(pattern.c_str());
+  TTree *oldtree = (TTree*)oldfile->Get("analysistree/anatree");
+  TFile *newfile = new TFile("selectedEntries.root","recreate");
+  TTree *newtree = oldtree->CloneTree(0);
+
+
   int selectedEvents = 0.;
   int counter = 0.;  
 
@@ -89,7 +154,11 @@ int main(int argc, char* argv[]) {
 
   Spectrum2D* Sfls_timeVSpe = new Spectrum2D("fls_timeVSpe",  ";Flash Time [#mus]; Flash PE",             2000, -10, 40, 5000, 0, 5000, totalPOT);
 
-  for(int i = 0; i < evts; i++) {
+  // This is a map: cutname <-> spectrum array
+  std::map<std::string,CutPlots> cutToPlotsMap;
+  InstantiateIntermidiatePlots(cutToPlotsMap,totalPOT);
+
+  for(int i = 0; i < 301/*evts*/; i++) {
 
     if(i%100 == 0) cout << "\t... " << i << endl;
 
@@ -130,10 +199,10 @@ int main(int argc, char* argv[]) {
     //
     // ************************
 
+    // Flash tag
     int theFlash = -1;
     if (selection->FlashTag(theFlash) == false) continue;
-
-    counter++;
+    MakeIntermidiatePlots(cutToPlotsMap.find("flashtag")->second,anatree);
  
     // Create vertex-track association
     std::map< int,std::vector<int> > VertexTrackCollection;
@@ -145,6 +214,7 @@ int main(int argc, char* argv[]) {
 
     // Check if vertex candidate is contained in FV
     if (!selection->InFV("vertex", vertexCandidate)) continue;
+    MakeIntermidiatePlots(cutToPlotsMap.find("vertexcontained")->second,anatree);
 
     // Get the longest track associated with the best vertex
     int trackCandidate;
@@ -162,6 +232,7 @@ int main(int argc, char* argv[]) {
     if (!selection->IsLongTrack(trackCandidate)) continue;
 
     selectedEvents++;
+    newtree->Fill();
  
     delete selection;
   }
@@ -176,6 +247,12 @@ int main(int argc, char* argv[]) {
 
   Sgeanttruetime_neutrino  ->Save();
   Sgeanttruetime_cosmic    ->Save();
+ 
+  SaveIntermidiatePlots(cutToPlotsMap);
+
+  newtree->AutoSave();
+  delete oldfile;
+  delete newfile;
 
   cout << endl << endl << "********************************" << endl;
   cout << "Total events:     " << evts << endl;
